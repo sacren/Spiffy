4d104126f4d073e3465624ba90e2423bb3fb129a
Jean Sacren <sakiwit@gmail.com>
2025-02-25 04:45:32 -0700

    Fix Vite HMR breakage in Docker container
    The value of server.hmr.host defaults to localhost.  This breaks HMR
    when Vite dev server is running concurrently with PHP development server
    in a Docker container hosted on a remote Linux server.
    
    This is a bug and it is 100% reproducible in the above environment.  Yet
    it is very easy to fix by setting server.hmr.host to a valid URI rather
    than localhost.
    
    For Vite 6, server.cors must be set to true.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        cors: true,
+        hmr: {
+            host: 'laravel.local',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
