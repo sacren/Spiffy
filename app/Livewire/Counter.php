b6a79ba888e971d056761d0dce4ac8da73ffe304
Jean Sacren <sakiwit@gmail.com>
2025-03-05 17:36:34 -0700

    Add livewire counter
    Create counter view scaffolding using make:view.
    
    $ sail artisan make:view -- counter
    
       INFO  View [resources/views/counter.blade.php] created successfully.
    
    Build the counter view that uses livewire counter.  Update the route to
    use the newly built counter view.
    
    Create livewire counter scaffolding using make:livewire.
    
    $ sail artisan make:livewire -- counter
     COMPONENT CREATED  🤙
    
    CLASS: app/Livewire/Counter.php
    VIEW:  resources/views/livewire/counter.blade.php
    
    Build livewire Counter class that passes $count variable to the livewire
    counter view.

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public int $count = 6;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
