84a3ff152f5e195b02a310d939515d727cac5e4f
Jean Sacren <sakiwit@gmail.com>
2025-03-07 23:08:54 -0700

    Use model to bind input to count property
    Livewire wire:model directive binds the count property to the input
    field.  The .live denotes the live update, which sends property updates
    to the server as the user types into the input field.  This generates
    high volume network traffic.  We use it here as an example for the demo.

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,6 @@
 <div>
     <p>Current time: {{ now('America/Denver') }}</p>
     <p>{{ $count }}</p>
+    <input type="number" wire:model.live="count">
     <button wire:click="changeCount(64)">Change Count</button>
 </div>
