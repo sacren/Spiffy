cfe3cd4314a75d3119c06031289162d6c853a40a
Jean Sacren <sakiwit@gmail.com>
2025-03-08 16:19:49 -0700

    Use defer modifier of wire:model directive
    This is how wire:model.defer works.
    
    Initial State:
    
    The count is 6.
    The number is 0.
    
    The UI displays:
    
    Count: 6
    Input: 0
    
    User Interaction:
    
    The user types 10 into the input field.
    
    Due to deferred updates (wire:model), the number property is not yet
    updated.  It remains 0 until the next network request.
    
    Button Click:
    
    When the user clicks the button, wire:click calls the changeCount method
    with the current value of number, which is still 0.
    
    The changeCount method updates count to 0.
    
    UI Update:
    
    The UI now displays:
    
    Count: 0
    Input: 0
    
    Button Click Again:
    
    Now the changeCount method updates the count to 10.

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
 <div>
     <p>Current time: {{ now('America/Denver') }}</p>
     <p>{{ $count }}</p>
-    <input type="number" wire:model="number">
+    <input type="number" wire:model.defer="number">
     <button wire:click="changeCount({{ $number }})">Change Count</button>
 </div>
