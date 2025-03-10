ec87aeb065a466923887e7d92fd2c2f2f2518dda
Jean Sacren <sakiwit@gmail.com>
2025-03-09 23:44:36 -0600

    Add BirdWatch Livewire component
    Create the livewire component scaffolding using make:livewire.
    
    $ sail artisan make:livewire -- BirdWatch
     COMPONENT CREATED  🤙
    
    CLASS: app/Livewire/BirdWatch.php
    VIEW:  resources/views/livewire/bird-watch.blade.php
    
    Build the bird-watch view with a necessary form.

---
+++ resources/views/livewire/bird-watch.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <form action="">
+        <input type="">
+        <textarea id="" name="" cols="30" rows="10"></textarea>
+        <button>Add a new bird count entry</button>
+    </form>
+</div>
