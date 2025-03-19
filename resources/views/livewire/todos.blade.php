37f912d978d211ba4e1759f45802e156ae2670ef
Jean Sacren <sakiwit@gmail.com>
2025-03-18 20:03:04 -0600

    Use .live modifier to Livewire model
    With .live modifier added to Livewire model, the task shows up as the
    typing is going on.

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -2,7 +2,7 @@
     <form wire:submit.prevent="add">
         <!-- Input and Add Button -->
         <div class="flex space-x-2">
-            <input type="text" wire:model="task" placeholder="Add a new task"
+            <input type="text" wire:model.live="task" placeholder="Add a new task"
                 class="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
             <button type="submit"
                 class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
@@ -10,6 +10,10 @@
             </button>
         </div>
     </form>
+
+    <div class="mt-6">
+        <span class="px-4 py-2 bg-gray-50 border border-gray-200 rounded-lg hover:bg-gray-100 transition duration-150 ease-in-out">Current Task: {{ $task }}</span>
+    </div>
 
     <!-- Todo List -->
     <ul class="mt-6 space-y-2">
