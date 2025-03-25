4119fc19e5e65bda43391382e792835b9c54a70f
Jean Sacren <sakiwit@gmail.com>
2025-03-24 23:53:48 -0600

    Rebuild the body of app layout
    Add a navbar of links for home page, etc.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,22 @@
         <title>{{ $title ?? 'Page Title' }}</title>
         @vite(['resources/css/app.css'])
     </head>
-    <body>
-        {{ $slot }}
+    <body class="min-h-screen bg-gray-100">
+        <div class="flex">
+            <!-- Vertical Navigation -->
+            <nav class="w-48 min-h-screen bg-white shadow-md p-4">
+                <div class="flex flex-col space-y-4">
+                    <a href="/" class="px-4 py-2 hover:bg-gray-100 rounded">Home</a>
+                    <a href="/todos" class="px-4 py-2 hover:bg-gray-100 rounded">Todos</a>
+                    <a href="/counter" class="px-4 py-2 hover:bg-gray-100 rounded">Counter</a>
+                    <a href="/dummy" class="px-4 py-2 hover:bg-gray-100 rounded">Dummy</a>
+                </div>
+            </nav>
+
+            <!-- Main Content -->
+            <main class="flex-1 p-8">
+                {{ $slot }}
+            </main>
+        </div>
     </body>
 </html>
